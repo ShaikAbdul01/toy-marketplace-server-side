6d1cf09ec87dcf5a08d617378be6a75143a7ee6b
Shaik Abdul Ahad <118795198+ShaikAbdul01@users.noreply.github.com>
2023-05-19 02:23:35 +0600

    get all car

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 require("dotenv").config();
 const app = express();
@@ -9,14 +9,11 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 // toyCarPlaza
 // WU1voWpjEk33zGK6
 
-
-
-
-const uri = "mongodb+srv://toyCarPlaza:WU1voWpjEk33zGK6@cluster0.5yf9dzl.mongodb.net/?retryWrites=true&w=majority";
+const uri =
+  "mongodb+srv://toyCarPlaza:WU1voWpjEk33zGK6@cluster0.5yf9dzl.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -24,25 +21,33 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toyCarCollection = client.db("toyCar").collection("cars");
+
+    app.get("/cars", async (req, res) => {
+      const cursor = toyCarCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-
-
-
 
 app.get("/", (req, res) => {
   res.send("ToyCarPlaza server is running");
